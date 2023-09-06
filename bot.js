d3f75613222c9e54337210513c26e2733166abd1
alif-gunadi <alifgunadi1303@gmail.com>
2023-09-06 11:52:00 +0700

    perbaiki kode openai

--- bot.js
+++ bot.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
-const { Client, Collection, GatewayIntentBits, ActivityType } = require("discord.js");
-const { Configuration, OpenAIApi } = require('openai')
+const { Client, Collection, GatewayIntentBits, ActivityType, Options } = require("discord.js");
+const { OpenAI } = require('openai');
 const fs = require("fs");
+const { RateLimiter } = require('limiter');
 
 const token = process.env.TOKEN;
 const channelId = process.env.CHANNEL_ID;
 
 const client = new Client({ 
+    sweepers: {
+        ...Options.DefaultSweeperSettings,
+        messages: {
+            interval: 3600,
+            lifetime: 1800
+        }
+    },
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
@@ -44,54 +52,64 @@ client.on('ready', () => {
       });
 });
 
-const configuration = new Configuration({
+const prefix = "$";
+
+const openai = new OpenAI({
     apiKey: process.env.API_CHAT_AI
 });
 
-const prefix = "$";
-
-const openai = new OpenAIApi(configuration);
+const limiter = new RateLimiter({
+    tokensPerInterval: 4,
+    interval: 'second', 
+});
 
 client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
-    if (message.channel.id !== channelId) return;
-    if (message.content.startsWith(prefix)) return;
-
-    const mentionedUsers = message.mentions.users;
-    const botMentioned = mentionedUsers.has(client.user.id);
-    if (!botMentioned) return;
-
-    let conversationLog = [
-        { role: "system", content: "You are a MavenPeace chat bot, people call you is Maven and you are made by alifgunadi."}
-    ];
-
-    await message.channel.sendTyping();
-
-    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
-    prevMessages.reverse();
-    prevMessages.forEach((msg) => {
+    try {
+        await limiter.removeTokens(1);
+        if (message.author.bot) return;
+        if (message.channel.id !== channelId) return;
         if (message.content.startsWith(prefix)) return;
-        if (msg.author.id !== client.user.id && message.author.bot) return;
-        if (msg.author.id !== message.author.id) return;
+
+        const mentionedUsers = message.mentions.users;
+        const botMentioned = mentionedUsers.has(client.user.id);
+        if (!botMentioned) return;
+
+        let conversationLog = [
+            { role: "system", content: "You are a MavenPeace chat bot"}
+        ];
+
+        await message.channel.sendTyping();
+
+        let prevMessages = await message.channel.messages.fetch({ limit: 2 });
+        prevMessages.reverse();
+        prevMessages.forEach((msg) => {
+            if (message.content.startsWith(prefix)) return;
+            if (msg.author.id !== client.user.id && message.author.bot) return;
+            if (msg.author.id !== message.author.id) return;
+
+            conversationLog.push({
+                role: 'user',
+                content: msg.content,
+            })
+        });
 
         conversationLog.push({
-            role: "user",
-            content: msg.content,
-        })
-    })
+            role: 'user',
+            content: message.content,
+        });
 
-    conversationLog.push({
-        role: 'user',
-        content: message.content,
-    });
+        const result = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: conversationLog
+        });
 
-    const result = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: conversationLog
-    });
+        await message.reply(result.data.choices[0].message);
 
-    message.reply(result.data.choices[0].message);
+    } catch (error) {
+        console.log(error);
+    }   
 });
+
 
 client.on('guildMemberAdd', (member) => {
     updateMemberCount(member.guild);
@@ -138,7 +156,7 @@ async function updateMemberCount(guild) {
         } else {
             console.log('User checker channel not found');
         }
-    }, 50000);
+    }, 60000);
 };
 
 
